0c1851bad16f50549cc31a249990786ff7668e5c
Eneldo Serrata <eneldoserrata@gmail.com>
2016-03-23 17:01:10 -0400

    IMP

--- fleet_x_vehicle_gallery/models/fleet_vehicle_gallery.py
+++ fleet_x_vehicle_gallery/models/fleet_vehicle_gallery.py
@@ -16,6 +16,36 @@ class fleet_vehicle_gallery(models.Model):
     rear_view = fields.Binary(_('Rear View'), requried=True)
     odometer_view = fields.Binary(_('Odometer View'), requried=True)
 
+    doc_type = fields.Selection([("in", "CONOCIMIENTO DE RECIBO"), ("in", "CONOCIMIENTO DE ENTREGA")], string="Tipo de conduce", required=True)
+    to_partner_id = fields.Many2one("res.partner", "Sirvase entregar vehiculo descrito abajo a", required=True)
+    from_partner_id = fields.Many2one("res.partner", "Para recibir de", required=True)
+    description = fields.Many2many("gallery.descriptions", string="Concepto de entrega")
+
+
+    internal_mirror = fields.Boolean("Espejo retrovisor interno")
+    right_mirror = fields.Boolean("Espejo retrovisor derecho")
+    left_mirror = fields.Boolean("Espejo retrovisor izquierdo")
+    fuel_plug = fields.Boolean(u"Tapón de gasolina")
+    radiator_plug = fields.Boolean(u"Tapón de gasolina")
+    radio = fields.Boolean("Radio")
+    gato = fields.Boolean("Gato")
+    tire = fields.Selection([("new", "Nuevas"),("used","Usadas")],"Gomas")
+    r_tire = fields.Selection([("new", "Nuevas"),("used","Usadas")],"Goma de repuesta")
+    carpet = fields.Boolean(u"Alfómbra")
+    kit = fields.Boolean(u"Botiquin")
+    lighter = fields.Boolean(u"Encendedor")
+    front_turn_lamp = fields.Boolean(u"Luces direccionales delanteras")
+    back_turn_lamp = fields.Boolean(u"Luces direccionales traseras")
+    manual = fields.Boolean(u"Manual del usuario")
+    keys = fields.Boolean(u"Duplicado llave de encendido")
+    wheel_wrench = fields.Boolean(u"LLave de rueda")
+
+
+class GalleryDescriptions(models.Model):
+    _name = "gallery.descriptions"
+
+    name = fields.Char()
+
 class fleet_vehicle(models.Model):
     _inherit = "fleet.vehicle"
     
